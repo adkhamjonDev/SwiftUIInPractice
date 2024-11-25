85a83027bda36621e511353493e9064ce4eeaf53
adkhamjon <adkhamjon.rakhimov.dev@gmail.com>
2024-11-25 15:37:28 +0500

    Initial Commit

---
+++ SwiftUIInPractice/SwiftUIInPracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIInPracticeApp.swift
+//  SwiftUIInPractice
+//
+//  Created by Adkhamjon Rakhimov on 25/11/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIInPracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
